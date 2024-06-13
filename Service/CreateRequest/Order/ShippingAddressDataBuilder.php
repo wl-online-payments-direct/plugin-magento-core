cc6008061da3d96b412b8dfdfcf2649776c93699
WorldlineMagentoTeam <plugin.worldline@amasty.com>
2024-06-13 16:50:58 +0300

    Release 2.17.0

--- Service/CreateRequest/Order/ShippingAddressDataBuilder.php
+++ Service/CreateRequest/Order/ShippingAddressDataBuilder.php
@@ -8,6 +8,7 @@ use OnlinePayments\Sdk\Domain\AddressPersonalFactory;
 use OnlinePayments\Sdk\Domain\PersonalNameFactory;
 use OnlinePayments\Sdk\Domain\Shipping;
 use OnlinePayments\Sdk\Domain\ShippingFactory;
+use Worldline\PaymentCore\Api\AmountFormatterInterface;
 use Worldline\PaymentCore\Api\Service\CreateRequest\Order\ShippingAddressDataBuilderInterface;
 use Worldline\PaymentCore\Model\Quote\Address\Comparer;
 
@@ -36,25 +37,40 @@ class ShippingAddressDataBuilder implements ShippingAddressDataBuilderInterface
      */
     private $addressComparer;
 
+    /**
+     * @var AmountFormatterInterface
+     */
+    private $amountFormatter;
+
     public function __construct(
         ShippingFactory $shippingFactory,
         AddressPersonalFactory $addressPersonalFactory,
         PersonalNameFactory $personalNameFactory,
-        Comparer $addressComparer
+        Comparer $addressComparer,
+        AmountFormatterInterface $amountFormatter
     ) {
         $this->shippingFactory = $shippingFactory;
         $this->addressPersonalFactory = $addressPersonalFactory;
         $this->personalNameFactory = $personalNameFactory;
         $this->addressComparer = $addressComparer;
+        $this->amountFormatter = $amountFormatter;
     }
 
     public function build(CartInterface $quote): Shipping
     {
-        $shippingAddress = $quote->getShippingAddress();
         $shipping = $this->shippingFactory->create();
 
+        $this->buildShippingAddress($quote, $shipping);
+        $this->buildShippingCost($quote, $shipping);
+
+        return $shipping;
+    }
+
+    public function buildShippingAddress(CartInterface $quote, Shipping $shipping): void
+    {
+        $shippingAddress = $quote->getShippingAddress();
         if (!$shippingAddress) {
-            return $shipping;
+            return;
         }
 
         $name = $this->personalNameFactory->create();
@@ -76,7 +92,28 @@ class ShippingAddressDataBuilder implements ShippingAddressDataBuilderInterface
         } else {
             $shipping->setAddressIndicator(self::DIFFERENT_THAN_BILLING);
         }
+    }
 
-        return $shipping;
+    public function buildShippingCost(CartInterface $quote, Shipping $shipping): void
+    {
+        $shippingAddress = $quote->getShippingAddress();
+        if (!$shippingAddress) {
+            return;
+        }
+
+        $currencyCode = (string)$quote->getCurrency()->getQuoteCurrencyCode();
+
+        $shippingAmount = $this->amountFormatter->formatToInteger(
+            (float)$shippingAddress->getShippingInclTax(),
+            $currencyCode
+        );
+
+        $shippingTaxAmount = $this->amountFormatter->formatToInteger(
+            (float)$shippingAddress->getShippingTaxAmount(),
+            $currencyCode
+        );
+
+        $shipping->setShippingCost($shippingAmount - $shippingTaxAmount);
+        $shipping->setShippingCostTax($shippingTaxAmount);
     }
 }
