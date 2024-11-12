b6fd9f289c3417251f5f9d49e0d7234b19a2c52c
Marija <marija.ivic@logeecom.com>
2024-11-12 08:52:41 +0100

    Remove template name extension validation
    CS-6163

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -3,12 +3,5 @@ let config = {
         '*': {
             checkConnection: 'Worldline_PaymentCore/js/testconnection'
         }
-    },
-    config: {
-        mixins: {
-            'mage/validation': {
-                'Worldline_PaymentCore/js/system/config/validation-mixin': true
-            }
-        }
     }
 };
